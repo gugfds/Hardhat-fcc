1a4efdef8c984c22ec9323bae507a5e5ecc8c226
Andes22 <Ivander25062001@gmail.com>
2024-05-06 11:43:38 +0700

    Create block-count.js

---
+++ task/block-count.js
@@ -0,0 +1,13 @@
+const { task } = require("hardhat/config")
+
+task("block-number", "Prints the current block number").setAction(
+    /* line 5 & 6 are the same as line 7*/
+    // const blockTask = async function() => {} 
+    // async function blockTask() {}
+    async (taskArgs, hre) => {
+        const blockNumber = await hre.ethers.provider.getBlockNumber()
+        console.log(`Current block number ${blockNumber}`)
+    }
+)
+
+module.exports = {}
