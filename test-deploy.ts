00c39fddeaba546d8e01bd51bc5b637dbbc02f47
Andes22 <Ivander25062001@gmail.com>
2024-05-09 17:26:45 +0700

    Create test-deploy.ts

---
+++ test-deploy.ts
@@ -0,0 +1,39 @@
+import { ethers } from "hardhat"
+import { expect, assert } from "chai"
+import { SimpleStorage, SimpleStorage__factory } from "../typechain-types"
+
+// (below is the same as) describe("SimpleStorage", function () => {})
+describe("SimpleStorage", function () {
+    //use this
+    // let SimpleStorageFactory
+    // let simpleStorage
+
+    //or this
+    let simpleStorageFactory: SimpleStorage__factory,
+        simpleStorage: SimpleStorage
+
+    beforeEach(async function () {
+        simpleStorageFactory = (await ethers.getContractFactory(
+            "SimpleStorage",
+        )) as unknown as SimpleStorage__factory
+        simpleStorage = await simpleStorageFactory.deploy()
+    })
+
+    it("Should start with a favorite number of 0", async function () {
+        const currentValue = await simpleStorage.retrieve()
+        const expectedValue = "0"
+
+        assert.equal(currentValue.toString(), expectedValue)
+        // or
+        // expect(currentValue.toString()).to.equal(expectedValue)
+    })
+
+    it("Should update when we call store", async function () {
+        const expectedValue = "7"
+        const transactionResponse = await simpleStorage.store(expectedValue)
+        await transactionResponse.wait(1)
+
+        const currentValue = await simpleStorage.retrieve()
+        assert.equal(currentValue.toString(), expectedValue)
+    })
+})
