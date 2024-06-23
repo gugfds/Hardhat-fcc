3cb13a65dc293ad562c3516ef19b329e15b03824
Andes22 <Ivander25062001@gmail.com>
2024-06-23 23:47:44 +0700

    Update block-count.js

--- task/block-count.js
+++ task/block-count.js
@@ -1,7 +1,7 @@
 const { task } = require("hardhat/config")
 
 task("block-number", "Prints the current block number").setAction(
-    /* line 5 & 6 are the same as line 7*/
+    /* line 5 & 6 same as line 7*/
     // const blockTask = async function() => {} 
     async (taskArgs, hre) => {
         const blockNumber = await hre.ethers.provider.getBlockNumber()
